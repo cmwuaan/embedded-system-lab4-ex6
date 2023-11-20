ebaed2c740bab798c30078d73543e8f63e85b88a
cmwuaan <minhquanrbpk@gmail.com>
2023-11-20 17:34:50 +0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # embedded-system-lab4
+lab4
--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,59 @@
 #include <Arduino.h>
 
-const int analogPin = A0;
-const int ledCount = 12;
+#define sensorAnalogPin A0
 
-int ledPin[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+byte ledPins[] = {4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+byte pinCount;
+int analogValue = 0;
+int soundMaxLevel = 0;
+int delayTime = 0;
 
 void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(analogPin, INPUT);
-  for (int i = 0; i < ledCount; i++)
+  pinCount = sizeof(ledPins);
+  for (int i = 0; i < pinCount; i++)
   {
-    pinMode(ledPin[i], OUTPUT);
+    pinMode(ledPins[i], OUTPUT);
+    digitalWrite(ledPins[i], LOW);
   }
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  int sensorValue = analogRead(analogPin);
-  sensorValue = map(sensorValue, 0, 1023, 0, ledCount);
-
-  for (int i = 0; i < ledCount; i++)
+  analogValue = analogRead(sensorAnalogPin);
+  delay(10);
+  Serial.println(analogValue);
+  if (analogValue > soundMaxLevel)
   {
-    digitalWrite(ledPin[i], i < sensorValue ? HIGH : LOW);
+    soundMaxLevel = analogValue;
+  }
+  int ledIndex = map(analogValue, 0, soundMaxLevel, 0, pinCount);
+  for (int i = 0; i < pinCount; i++)
+  {
+    if (i < ledIndex)
+    {
+      digitalWrite(ledPins[i], HIGH);
+      if (i > 7)
+      {
+        delay(300);
+      }
+      else if (i <= 7 && i > 3)
+      {
+        delay(150);
+      }
+      else
+      {
+        delay(50);
+      }
+
+      digitalWrite(ledPins[i], LOW);
+    }
+    else
+    {
+      digitalWrite(ledPins[i], LOW);
+    }
   }
 }
